f15db1047d91de11d9e93110d10db0d830f4fa68
Colby <colby.l.howell@gmail.com>
2021-06-23 00:50:48 -0500

    changed some error handling

--- data/weather-calc-model.js
+++ data/weather-calc-model.js
@@ -2,49 +2,16 @@ const axios = require("axios");
 
 function getWeather(params) {
   return axios
-    .get(
-      `http://api.openweathermap.org/data/2.5/forecast?zip=${params}&appid=e05fcca67ee38ae893eff916b9f646f2`
+  .get( 
+      `http://api.openweathermap.org/data/2.5/forecast?zip=${params.query.zip}&appid=e05fcca67ee38ae893eff916b9f646f2&units=${params.query.unit}`
     )
     .then((res) => {
-      const tempData = [];
-      const city = res.data.city.name;
-      res.data.list.forEach((d) => {
-        tempData.push(d.main.temp);
+      const weatherData = [];
+      res.data.list.map((d) => {
+        weatherData.push(d.main.temp);
       });
 
-      const mean = (...meanArr) =>
-        meanArr.reduce((a, cv) => a + cv, 0) / meanArr.length;
-
-      const median = (medArr) => {
-        let midNum = Math.floor(medArr.length / 2);
-        let theMedian = 0;
-        medArr.sort((a, b) => a - b);
-        return medArr.length % 2 === 0
-          ? (medArr[midNum - 1] + medArr[midNum / 2]) / 2
-          : medArr[midNum];
-      };
-
-      const mode = (modeArr) => {
-        var numCounts = [];
-        var sortedArray = modeArr.sort((a, b) => a - b);
-        sortedArray.forEach((num, i, arr) => {
-          var compared = arr[i + 1];
-          if (num === compared) {
-            numCounts.push(num);
-          }
-        });
-
-        let theMode = [...new Set(numCounts)];
-
-        return theMode.toString();
-      };
-
-      return {
-        mean: mean(...tempData),
-        median: median(tempData),
-        mode: mode(tempData),
-        city: city,
-      };
+      return weatherData
     })
     .catch((err) => {
       console.log(err.response);
--- data/weather-calc-router.js
+++ data/weather-calc-router.js
@@ -3,12 +3,24 @@ const router = express.Router();
 
 const Weather = require("./weather-calc-model");
 
-router.get("/:zip", async (req, res) => {
-  const zip = req.params.zip;
+router.get("/zip", async (req, res) => {
+
+  const impOrMet = function(arr){
+    if(arr.query.unit == 'Farenheit'){
+      let unitVal = 'imperial'
+      return unitVal
+    }else{
+      let unitVal = 'metric'
+      return unitVal
+    }
+  }
+    req.query.unit = impOrMet(req)
   try {
-    const data = await Weather.getWeather(zip);
-    res.status(200).json(data);
-  } catch {
+    const data = await Weather.getWeather(req);
+    if(data.status === 404){
+      throw new TypeError("Data not found")
+    }else res.status(200).json(data);
+  } catch(e) {
     res.status(404).json({ Message: "The data could not be found" });
   }
 });
